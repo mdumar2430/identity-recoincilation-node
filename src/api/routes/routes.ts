baee1b96e398957533cea00e2fe46e3ad03edc0d
Mohamad Umar M <81101575+mdumar2430@users.noreply.github.com>
2025-07-18 23:13:01 +0530

    Update routes.ts

--- src/api/routes/routes.ts
+++ src/api/routes/routes.ts
@@ -1,4 +1,13 @@
 import { ServerRoute } from "@hapi/hapi";
+import {
+  createContactPayloadSchema,
+  createContactHandler,
+} from "../handlers/contacts/create.js";
+import { getContactsHandler } from "../handlers/contacts/getAll.js";
+import {
+  identifyPayloadSchema,
+  identifyHandler,
+} from "../handlers/contacts/identify.js";
 
 export const routes: ServerRoute[] = [
   {
@@ -10,10 +19,39 @@ export const routes: ServerRoute[] = [
   },
   {
     method: "POST",
-    path: "/identify",
-    handler: (request, h) => {
-      return "This is the about page.";
+    path: "/contacts",
+    options: {
+      description: "Create a contact",
+      validate: {
+        payload: createContactPayloadSchema,
+        failAction: (request, h, err) => {
+          throw err;
+        },
+      },
+      handler: createContactHandler,
     },
   },
-  // Add more routes as needed
+  {
+    method: "GET",
+    path: "/contacts",
+    options: {
+      description: "Get all contacts",
+      handler: getContactsHandler,
+    },
+  },
+  {
+    method: "POST",
+    path: "/identify",
+    options: {
+      tags: ["api"],
+      description: "Identifies and merges user contacts",
+      validate: {
+        payload: identifyPayloadSchema,
+        failAction: (request, h, err) => {
+          throw err;
+        },
+      },
+      handler: identifyHandler,
+    },
+  },
 ];
