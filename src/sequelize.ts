a8db02ce661cbad7238b9aa179d37bce7ed610ba
Mohamad Umar M <81101575+mdumar2430@users.noreply.github.com>
2025-07-18 23:51:22 +0530

    added .env

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -1,7 +1,15 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-export const sequelize = new Sequelize('postgres', 'postgres', 'passw0rd', {
-  host: 'localhost',
-  dialect: 'postgres',
-  logging:false
-});
+dotenv.config();
+
+export const sequelize = new Sequelize(
+  process.env.DB_NAME || "",
+  process.env.DB_USER || "",
+  process.env.DB_PASS || "",
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT as any,
+    logging: false
+  }
+);
